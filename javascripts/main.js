346798ef138e8726482fe394063bb743d6e1bde6
ahmad barutchi <ahmad.barutchi@gmail.com>
2016-06-21 14:44:51 +0200

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,4 @@
 console.log('This would be the main JS file.');
+navigator.registerProtocolHandler("mailto",
+                                  "https://mail.google.com/mail/?extsrc=mailto&url=%s",
+                                  "Gmail");
